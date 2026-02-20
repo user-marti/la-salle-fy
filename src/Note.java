c891c3675e946be1291082a46dcb43c1a16ce5de
Marti Quera <marti.quera@gracia.lasalle.cat>
2026-02-20 10:11:46 +0100

    classe 'note' acabada

--- src/Note.java
+++ src/Note.java
@@ -1,2 +1,24 @@
 public class Note {
+    private double frecuencia; // en herzios
+    private int tiempo;       // en milisegundos
+    private WaveType wt;       // timbre
+
+    // Constructor principal
+    public Note(double frecuencia, int tiempo, WaveType wt) {
+        this.frecuencia = frecuencia;
+        this.tiempo = tiempo;
+        this.wt = wt;
+    }
+
+    public Note(int durationMs) {
+        this(0, durationMs, WaveType.SILENCE);
+    }
+
+    public double getFrecuencia() { return frecuencia; }
+    public int getTiempo() { return tiempo; }
+    public WaveType getWt() { return wt; }
+
+    public boolean isSilence() {
+        return this.wt == WaveType.SILENCE;
+    }
 }

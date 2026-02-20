80b3e4c359d9483b8fb49376e67476c887eca384
Marti Quera <marti.quera@gracia.lasalle.cat>
2026-02-20 10:15:49 +0100

    classe 'song' acabada

--- src/Song.java
+++ src/Song.java
@@ -1,2 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Song {
+    private String id, title, artist, style;
+    private int durationSeconds;
+    private Mood mood;
+    private List<Note> notes = new ArrayList<>();
+
+    public Song(String id, String title, String artist, int durationSeconds, Mood mood, String style) {
+        this.id = id;
+        this.title = title;
+        this.artist = artist;
+        this.durationSeconds = durationSeconds;
+        this.mood = mood;
+        this.style = style;
+    }
+
+    // Mètodes
+    public void addNote(Note n) { notes.add(n); }
+
+    public boolean isPlayable() { return !notes.isEmpty(); } //
+
+    // Getters
+    public String getId() { return id; }
+    public String getTitle() { return title; }
+    public List<Note> getNotes() { return notes; }
+
+
+    @Override
+    public String toString() {
+        String tag = isPlayable() ? "[PLAYABLE]" : "[NOT]"; //
+        return tag + " " + title + " - " + artist;
+    }
 }
